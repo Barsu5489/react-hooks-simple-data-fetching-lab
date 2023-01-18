6b2d1b429547959d6df2ef95878b0080e07fd22f
barsu5489 <ebarsulai@gmail.com>
2023-01-18 14:34:18 +0300

    random dog

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useEffect, useState } from 'react'
+
+function App() {
+const [dog,setDog]= useState(null)
+
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setDog(data)
+        })
+
+    },)
+  if(dog===null){
+    return <p>Loading...</p>
+  }
+  console.log(dog)
+  return (
+    <div>
+        <img src={dog.message} alt="A Random Dog" />
+        
+    </div>
+  )
+}
+
+export default App
